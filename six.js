e4b3d0d00bcd84d79b847485078fa0daa4856f69
Sachin Shelar <sachinshelar@Sachins-MacBook-Pro.local>
2021-06-13 17:02:14 +0530

    Six commit

---
+++ six.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class six extends Component {
+    render() {
+        return (
+            <div>
+                || Shree Swami Samarth ||
+            </div>
+        )
+    }
+}
