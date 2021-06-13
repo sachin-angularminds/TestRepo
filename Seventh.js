f59b7bba829ef6cb4f7039ddaf3e9f5ab0dce34f
Sachin Shelar <sachinshelar@Sachins-MacBook-Pro.local>
2021-06-13 17:13:56 +0530

    My Seventh changes

---
+++ Seventh.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Seventh extends Component {
+    render() {
+        return (
+            <div>
+                || SSS ||
+            </div>
+        )
+    }
+}
